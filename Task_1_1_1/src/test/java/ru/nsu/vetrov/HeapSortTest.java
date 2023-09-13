c8d3fa4b432751a7fa781672d045026915c13def
justProductAnalyst <v7vqwerty@gmail.com>
2023-09-13 22:56:07 +0700

    Rearrange imports

--- Task_1_1_1/src/test/java/ru/nsu/vetrov/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/vetrov/HeapSortTest.java
@@ -1,8 +1,8 @@
 package ru.nsu.vetrov;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Class tests the HeapSort class.
