6aeaa06be0ee427bf8fe27a828a8a36c782da00a
justProductAnalyst <v7vqwerty@gmail.com>
2023-11-18 22:50:16 +0700

    Add more tests, fix algorithm, add test with large file

--- Task_1_3_1/src/main/java/ru/nsu/vetrov/SubstringSearcher.java
+++ Task_1_3_1/src/main/java/ru/nsu/vetrov/SubstringSearcher.java
@@ -1,9 +1,8 @@
 package ru.nsu.vetrov;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,43 +14,58 @@ public class SubstringSearcher {
     /**
      * Finds all occurrences of a substring within a text file and returns their starting indices.
      *
-     * @param filename  The name of the file to search within.
-     * @param substring The substring to find.
+     * @param resourceName The name of the file to search within.
+     * @param substring    The substring to find.
      * @return A list of starting indices of each occurrence of the substring.
      * @throws IOException If an I/O error occurs.
      */
-    public static List<Integer> find(String filename, String substring) throws IOException {
+    public static List<Integer> find(String resourceName, String substring) throws IOException {
         List<Integer> indices = new ArrayList<>();
-        File file = new File(filename);
-        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
-            byte[] buffer = new byte[4096];
-            boolean overlap = false;
-            int overlapSize = substring.length() - 1;
-            int bytesRead;
-            int position = 0;
-
-            while ((bytesRead = bis.read(buffer)) != -1) {
-                String chunk = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
-
-                if (overlap) {
-                    int overlapStart = Math.max(0, position - overlapSize);
-                    String overlapChunk = new String(buffer, overlapStart,
-                            overlapSize, StandardCharsets.UTF_8);
-                    chunk = overlapChunk + chunk;
-                }
-
-                int index = -1;
-                while ((index = chunk.indexOf(substring, index + 1)) != -1) {
-                    if (overlap && index < overlapSize) {
-                        continue;
-                    }
-                    indices.add(position + index - (overlap ? overlapSize : 0));
-                }
-
-                position += bytesRead;
-                overlap = true;
+        try (InputStream is = SubstringSearcher.class.getClassLoader().getResourceAsStream(
+                resourceName)
+        ) {
+            if (is == null) {
+                throw new IOException("Resource not found: " + resourceName);
             }
+            try (BufferedInputStream bis = new BufferedInputStream(is)) {
+                byte[] buffer = new byte[4096];
+                int overlapSize = substring.length() - 1;
+                int bytesRead;
+                int position = 0;
+                String previousEnd = "";
+
+                while ((bytesRead = bis.read(buffer)) != -1) {
+                    String chunk = previousEnd + new String(
+                            buffer,
+                            0,
+                            bytesRead,
+                            StandardCharsets.UTF_8
+                    );
+
+                    int index = -1;
+                    while ((index = chunk.indexOf(substring, index + 1)) != -1) {
+                        indices.add(position + index);
+                    }
+
+                    if (bytesRead >= overlapSize) {
+                        previousEnd = new String(
+                                buffer,
+                                bytesRead - overlapSize,
+                                overlapSize, StandardCharsets.UTF_8
+                        );
+                    } else {
+                        previousEnd = new String(
+                                buffer,
+                                0,
+                                bytesRead,
+                                StandardCharsets.UTF_8
+                        );
+                    }
+
+                    position += bytesRead - overlapSize > 0 ? bytesRead - overlapSize : 0;
+                }
+            }
+            return indices;
         }
-        return indices;
     }
 }
--- Task_1_3_1/src/test/java/ru/nsu/vetrov/SubstringSearcherTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/vetrov/SubstringSearcherTest.java
@@ -1,62 +1,39 @@
 package ru.nsu.vetrov;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test class for SubstringSearcher.
+ * It verifies the functionality of finding substrings within files loaded via getResourceAsStream.
  */
 public class SubstringSearcherTest {
 
-    private static final String TEST_FILE_NAME = "testInput.txt";
+    private static final String TEST_RESOURCE_NAME = "testInput.txt";
     private static Path testFilePath;
 
     /**
-     * Sets up the test environment before all tests.
-     *
-     * @throws IOException if an I/O error occurs
-     */
-    @BeforeAll
-    public static void setUp() throws IOException {
-        testFilePath = Files.createTempFile(null, TEST_FILE_NAME);
-        // Write to the file using UTF-8 encoding
-        try (BufferedWriter writer = Files.newBufferedWriter(
-                testFilePath,
-                StandardCharsets.UTF_8)
-        ) {
-            writer.write("абракадабра");
-        }
-    }
-
-    /**
-     * Cleans up the test environment after all tests.
-     *
-     * @throws IOException if an I/O error occurs
-     */
-    @AfterAll
-    public static void tearDown() throws IOException {
-        Files.deleteIfExists(testFilePath);
-    }
-
-    /**
      * Tests the find method for a multi-line text file.
+     *
+     * @throws IOException if an I/O error occurs
      */
     @Test
     public void testFindSubstringInMultiLineText() throws IOException {
-        Path filePath = Paths.get("main/resources/multiLineText.txt");
-        List<Integer> expectedIndices = Arrays.asList(10, 19, 42);
-        List<Integer> actualIndices = SubstringSearcher.find(filePath.toString(), "test");
+        List<Integer> expectedIndices = Arrays.asList(10, 47);
+        List<Integer> actualIndices = SubstringSearcher.find(
+                "multiLineTest.txt",
+                "test"
+        );
         assertEquals(
                 expectedIndices,
                 actualIndices,
@@ -66,12 +43,16 @@ public class SubstringSearcherTest {
 
     /**
      * Tests the find method for a text file with repeated patterns.
+     *
+     * @throws IOException if an I/O error occurs
      */
     @Test
     public void testFindSubstringInRepeatedPatterns() throws IOException {
-        Path filePath = Paths.get("main/resources/repeatedPatterns.txt");
         List<Integer> expectedIndices = Arrays.asList(0, 2, 4, 6);
-        List<Integer> actualIndices = SubstringSearcher.find(filePath.toString(), "aba");
+        List<Integer> actualIndices = SubstringSearcher.find(
+                "repeatedPatterns.txt",
+                "aba"
+        );
         assertEquals(
                 expectedIndices,
                 actualIndices,
@@ -79,18 +60,42 @@ public class SubstringSearcherTest {
         );
     }
 
-
+    /**
+     * Tests the find method for a very large text file (15GB).
+     * Note: This test is commented out due to its resource-intensive nature.
+     *
+     * @throws IOException if an I/O error occurs
+     */
     @Test
-    public void testFindSubstring() throws IOException {
-        List<Integer> expectedIndices = Arrays.asList(1, 8);
-        List<Integer> actualIndices = SubstringSearcher.find(
-                testFilePath.toString(),
-                "бра"
-        );
+    public void veryLargeFileTest() {
+        String fileName = "src/test/resources/large_file.txt";
+        long fileSize = 1024L * 1024 * 1024 * 8; // 8GB
+        List<Integer> res;
+
+        File file = new File(fileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write("010");
+            for (long ind = 3; ind < fileSize; ind++) {
+                writer.write('0');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            res = SubstringSearcher.find("large_file.txt", "10");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to find substring in the file", e);
+        } finally {
+            file.delete();
+        }
+
+        List<Integer> expected = Arrays.asList(1);
+
         assertEquals(
-                expectedIndices,
-                actualIndices,
-                "The indices should match the expected values."
+                expected,
+                res,
+                "The index should match the expected value in the large file."
         );
     }
 }
