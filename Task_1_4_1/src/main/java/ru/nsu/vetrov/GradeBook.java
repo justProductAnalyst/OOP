daa330fc977ae5fb03f81524e5d52e01aa8eb3fd
justProductAnalyst <v7vqwerty@gmail.com>
2023-11-21 19:15:38 +0700

    Fix build and add javadoc

--- Task_1_4_1/src/main/java/ru/nsu/vetrov/GradeBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/vetrov/GradeBook.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Represents an electronic grade book for a student, with functionalities
+ * to manage grades and evaluate academic achievements.
+ */
 public class GradeBook {
     private String studentName;
     private int currentSemester;
@@ -13,24 +17,52 @@ public class GradeBook {
     private static final int EXCELLENT_GRADE = 5;
     private static final int SATISFACTORY_GRADE = 3;
 
+    /**
+     * Constructs a new GradeBook with initial values.
+     *
+     * @param studentName The name of the student.
+     * @param initialSemester The initial semester of the student.
+     */
     public GradeBook(String studentName, int initialSemester) {
         this.studentName = studentName;
         this.currentSemester = initialSemester;
         this.grades = new ArrayList<>();
     }
 
+    /**
+     * Adds a grade to the grade book.
+     *
+     * @param grade The grade achieved.
+     * @param subject The subject for which the grade was achieved.
+     * @param semester The semester in which the grade was achieved.
+     */
     public void addGrade(int grade, String subject, int semester) {
         grades.add(new Grade(grade, subject, semester));
     }
 
+    /**
+     * Sets the grade for the final qualifying work.
+     *
+     * @param grade The grade of the final qualifying work.
+     */
     public void setFinalQualifyingWorkGrade(int grade) {
         this.finalQualifyingWorkGrade = new Grade(grade, "Final Qualifying Work", currentSemester);
     }
 
+    /**
+     * Calculates the average grade across all semesters.
+     *
+     * @return The average grade.
+     */
     public double calculateAverageGrade() {
         return grades.stream().mapToInt(Grade::getGrade).average().orElse(0.0);
     }
 
+    /**
+     * Determines if the student is eligible for honors.
+     *
+     * @return True if eligible for honors, false otherwise.
+     */
     public boolean isEligibleForHonors() {
         long excellentGradesCount = grades.stream().filter(g -> g.getGrade() == EXCELLENT_GRADE).count();
         boolean hasSatisfactoryGrade = grades.stream().anyMatch(g -> g.getGrade() == SATISFACTORY_GRADE);
@@ -39,33 +71,62 @@ public class GradeBook {
         return excellentGradesCount >= grades.size() * 0.75 && !hasSatisfactoryGrade && finalWorkIsExcellent;
     }
 
+    /**
+     * Determines if the student is eligible for an increased scholarship
+     * based on the grades of the current semester.
+     *
+     * @return True if eligible for increased scholarship, false otherwise.
+     */
     public boolean isEligibleForIncreasedScholarship() {
         List<Grade> lastSemesterGrades = grades.stream()
                 .filter(g -> g.getSemester() == currentSemester)
-                .toList();
+                .collect(Collectors.toList());;
 
         return lastSemesterGrades.stream().allMatch(g -> g.getGrade() == EXCELLENT_GRADE);
     }
 
+    /**
+     * Increments the current semester.
+     */
     public void incrementSemester() {
         currentSemester++;
     }
 
+    /**
+     * Represents a grade for a specific subject in a specific semester.
+     */
     private static class Grade {
         private int grade;
         private String subject;
         private int semester;
 
+        /**
+         * Constructs a new Grade.
+         *
+         * @param grade The grade achieved.
+         * @param subject The subject for which the grade was achieved.
+         * @param semester The semester in which the grade was achieved.
+         */
         public Grade(int grade, String subject, int semester) {
             this.grade = grade;
             this.subject = subject;
             this.semester = semester;
         }
 
+        /**
+         * Gets the grade.
+         *
+         * @return The grade.
+         */
         public int getGrade() {
             return grade;
         }
 
+        /**
+         * Gets the semester of the grade.
+         *
+         * @return The semester.
+         */
         public int getSemester() {
             return semester;
         }
--- Task_1_4_1/src/test/java/ru/nsu/vetrov/GradeBookTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/vetrov/GradeBookTest.java
@@ -7,18 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 /**
- * Test class for the GradeBook class.
+ * Provides unit tests for the GradeBook class, testing its various functionalities
+ * including grade management, average grade calculation, and eligibility checks for honors
+ * and increased scholarship.
  */
 public class GradeBookTest {
     private GradeBook gradeBook;
 
+    /**
+     * Sets up the test environment before each test.
+     * Initializes a new instance of GradeBook for testing.
+     */
     @BeforeEach
     public void setUp() {
         gradeBook = new GradeBook("John Doe", 1);
     }
 
+    /**
+     * Tests the addition of grades and the calculation of the average grade.
+     * Verifies that the average is calculated correctly based on the added grades.
+     */
     @Test
     public void testAddAndCalculateAverageGrade() {
         gradeBook.addGrade(4, "Math", 1);
@@ -29,11 +38,13 @@ public class GradeBookTest {
         assertEquals(4.0, avg, 0.01);
     }
 
+    /**
+     * Tests the eligibility for honors.
+     * Verifies that the student is eligible for honors when all conditions are met.
+     */
     @Test
     public void testEligibilityForHonors() {
-        // Assuming final qualifying work grade needs to be set for eligibility
         gradeBook.setFinalQualifyingWorkGrade(5);
-
         gradeBook.addGrade(5, "Math", 1);
         gradeBook.addGrade(5, "Science", 1);
         gradeBook.addGrade(5, "History", 1);
@@ -41,17 +52,24 @@ public class GradeBookTest {
         assertTrue(gradeBook.isEligibleForHonors());
     }
 
+    /**
+     * Tests the ineligibility for honors due to a satisfactory grade.
+     * Verifies that the student is not eligible for honors if they have a satisfactory grade.
+     */
     @Test
     public void testIneligibilityForHonorsDueToSatisfactoryGrade() {
         gradeBook.setFinalQualifyingWorkGrade(5);
-
-        gradeBook.addGrade(3, "Math", 1); // Satisfactory grade
+        gradeBook.addGrade(3, "Math", 1);
         gradeBook.addGrade(5, "Science", 1);
         gradeBook.addGrade(5, "History", 1);
 
         assertFalse(gradeBook.isEligibleForHonors());
     }
 
+    /**
+     * Tests the eligibility for an increased scholarship based on excellent grades in the last semester.
+     * Verifies that the student is eligible for an increased scholarship when all grades in the last semester are excellent.
+     */
     @Test
     public void testEligibilityForIncreasedScholarship() {
         gradeBook.addGrade(5, "Math", 1);
@@ -60,20 +78,27 @@ public class GradeBookTest {
         assertTrue(gradeBook.isEligibleForIncreasedScholarship());
     }
 
+    /**
+     * Tests the ineligibility for an increased scholarship due to a non-excellent grade.
+     * Verifies that the student is not eligible for an increased scholarship if any grade in the last semester is not excellent.
+     */
     @Test
     public void testIneligibilityForIncreasedScholarshipDueToNonExcellentGrade() {
-        gradeBook.addGrade(4, "Math", 1); // Not an excellent grade
+        gradeBook.addGrade(4, "Math", 1);
         gradeBook.addGrade(5, "Science", 1);
 
         assertFalse(gradeBook.isEligibleForIncreasedScholarship());
     }
 
+    /**
+     * Tests the functionality of incrementing the current semester.
+     * Verifies that grades added after incrementing the semester are associated with the new semester.
+     */
     @Test
     public void testIncrementSemester() {
         gradeBook.incrementSemester();
         gradeBook.addGrade(5, "Math", 2);
 
-        // The added grade should be in the second semester
         assertTrue(gradeBook.isEligibleForIncreasedScholarship());
     }
 }
