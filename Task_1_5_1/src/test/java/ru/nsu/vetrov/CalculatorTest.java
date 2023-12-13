1fc2411bcef28b5c4e8dabda60ace20461e351c0
justProductAnalyst <v7vqwerty@gmail.com>
2023-12-13 21:31:04 +0700

    Add Exceptions

--- Task_1_5_1/src/test/java/ru/nsu/vetrov/CalculatorTest.java
+++ Task_1_5_1/src/test/java/ru/nsu/vetrov/CalculatorTest.java
@@ -1,9 +1,10 @@
 package ru.nsu.vetrov;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test class for the calculator functionality.
