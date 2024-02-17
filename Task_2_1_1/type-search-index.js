98bf20df845fd49f1a42ff256f084367ab1636be
j4stV <j4stV@users.noreply.github.com>
2024-02-17 09:29:24 +0000

    Deploying to gh-pages from @ j4stV/OOP@14aab19b209c6a6bfce392294da7e67972636d06 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.vetrov","l":"ExcludeFromJacocoGeneratedReport"},{"p":"ru.nsu.vetrov","l":"PrimeChecker"},{"p":"ru.nsu.vetrov","l":"PrimeCheckerParallel"},{"p":"ru.nsu.vetrov","l":"PrimeCheckerParallelStream"}]
